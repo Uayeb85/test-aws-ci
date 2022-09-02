c017ad34d923e6ae21a74096530854a4ef7a2210
uayeb85 <gustavo.aa18@gmail.com>
2022-09-02 15:50:33 -0400

    test2

--- logic.js
+++ logic.js
@@ -3,9 +3,9 @@ const countEl = document.getElementById('count');
 updateVisitCount();
 
 function updateVisitCount() {
-	fetch('https://api.countapi.xyz/update/iamgus/aws/?amount=1')
-	.then(res => res.json())
-	.then(res => {
-		countEl.innerHTML = res.value;
-	})
+fetch('https://api.countapi.xyz/update/iamgus/aws/?amount=1')
+.then(res => res.json())
+.then(res => {
+countEl.innerHTML = res.value;
+})
 }
